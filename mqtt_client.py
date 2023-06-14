958c7692f533fa2ff3ea439d7966f305f1885911
nakyeonko3 <badaclock@gmail.com>
2023-06-14 11:02:16 +0900

    first commit

---
+++ mqtt_client.py
@@ -0,0 +1,59 @@
+import paho.mqtt.client as mqtt
+import create_csvfile
+
+# from create_csv import append_csvfile
+
+# 구독할 토픽명, 브로커 주소
+TOPIC = "power_value"
+HOSTNAME = "nakyeonko3.local"
+# HOSTNAME = "localhost"
+
+last_data = 0
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=", rc)
+
+
+def on_disconnect(client, userdata, flags, rc=0):
+    print(str(rc))
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_message(client, userdata, msg):
+    global last_data
+    data = int(msg.payload.decode("utf-8"))
+    gap = last_data - data
+
+    if data <= 385:
+        data = 0
+
+    if gap >= 1000:
+        data = last_data
+    create_csvfile.append_csvfile(data)
+    print(data)
+    last_data = data
+
+    # json_dict = json.loads(data)
+    # eletric_current = json_dict["eletric current"]
+    # append_csvfile(eletric_current)
+
+
+client = mqtt.Client()
+
+# 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료),on_subscribe(topic 구독)
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+
+client.connect(HOSTNAME, 1883)
+
+client.subscribe(TOPIC, 1)
+client.loop_forever()
